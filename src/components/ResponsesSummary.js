c8ae4a16d92747addb9e5797a144491b621c0645
Leukar03 <raquelclementediaz03@gmail.com>
2025-06-03 09:15:32 +0200

    cambios redaccion3

--- src/components/ResponsesSummary.js
+++ src/components/ResponsesSummary.js
@@ -154,7 +154,7 @@ const ResponsesSummary = ({ responses, event }) => {
               dependencia = `Dependiente de <b>${estructura}</b> <b>${lado}</b>`;
             }
             const contenido = MA_CONTENIDO === 'otro' ? MA_CONTENIDO_OTRO : {MA_CONTENIDO};
-            report += `${dependencia}, se objetiva formación de <b>${MA_M1} x ${MA_M2} x ${MA_M3} mm</b> <b>(${MA_VOL} cm³)</b> de aspecto <b>${MA_TIPO}</b> de contorno <b>${contorno}</b> y de contenido <b>${contenido}</b>.${vascularizacion_MA_SOL}.<br/>`;
+            report += `HOLAAAAA ${dependencia}, se objetiva formación de <b>${MA_M1} x ${MA_M2} x ${MA_M3} mm</b> <b>(${MA_VOL} cm³)</b> de aspecto <b>${MA_TIPO}</b> de contorno <b>${contorno}</b> y de contenido <b>${contenido}</b>.${vascularizacion_MA_SOL}.<br/>`;
           
             // Información adicional para masas quísticas y sólido-quísticas
             let vascularizacion_MA_Q = '';	
