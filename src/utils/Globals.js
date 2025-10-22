8cd05b3759686cd4c1c0df22cf059d0a18f6c259
fjgarate <fgarate.dev@gmail.com>
2025-10-22 17:24:38 +0200

    g

--- src/utils/Globals.js
+++ src/utils/Globals.js
@@ -1,6 +1,6 @@
 module.exports = {
   STORE_KEY: 'a56z0fzrNpl^2',
-  BASE_URL: 'https://emma.gbt.tfo.upm.es:8443/api/mobil',
+  BASE_URL: 'https://emma.gbt.tfo.upm.es:8443',
  // BASE_URL: 'http://localhost:8081',
   //  BASE_URL: 'https://emma.gbt.tfo.upm.es',
 };
